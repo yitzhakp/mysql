4b60c2f34be03150a8054bd41b63288cba75ddcf
Yitzhak P. J. <yitzhakp@uninorte.edu.co>
2024-03-30 11:36:45 -0500

    Fi dependencies 3

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.post('/api/notes', (request, response) => {
   response.status(201).json(newNote)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
